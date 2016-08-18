5760d523d53dea25737e7d6685f4e1272e96373a
Lyman <limianfeng12@gmail.com>
2016-08-18 20:55:24 +0800

    添加log工具类

---
+++ Assets/Script/ToolScript/LogScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LogScript : MonoBehaviour {
+
+	//log开关
+	static public bool isOpen = true; 
+
+	public Text logText;
+
+	private string currentContent;
+
+	void Start () {
+	
+		logText.enabled = isOpen;
+		currentContent = logText.text;
+	}
+
+	void Update () {
+	
+		if (!logText.text.Equals (currentContent)) {
+
+			logText.text = currentContent;
+		}
+	}
+
+	public void Log(string log) {
+
+		currentContent += "\n" +log;
+	}
+}
