5e5b665a2b4fd9e8137213bc710928d2e86d2e18
Lyman <limianfeng12@gmail.com>
2016-08-18 22:52:17 +0800

    添加屏幕工具类

---
+++ Assets/Script/SpriteScript/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public GameObject logLogic;
+	public GameObject screenLogic;
+
+	private Vector2 loc;
+	private Vector2 size;
+
+	// Use this for initialization
+	void Start () {
+
+		size = screenLogic.GetComponent<ScreenScript> ().getScreenWorldSize();
+		loc = screenLogic.GetComponent<ScreenScript> ().getScreenWorldLoc();
+
+		logLogic.GetComponent<LogScript> ().Log ("" + size.x + " " + size.y);
+
+		logLogic.GetComponent<LogScript> ().Log ("" + loc.x + " " + loc.y);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Script/ToolScript/ScreenScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//获取屏幕的世界尺寸
+	public Vector2 getScreenWorldSize() {
+
+		return Camera.main.ScreenToWorldPoint (new Vector2(Screen.width, Screen.height));
+	}
+
+	//获取屏幕的世界位置
+	public Vector2 getScreenWorldLoc() {
+
+		return Camera.main.ScreenToWorldPoint (new Vector2(0, 0));
+	}
+}
