ff24c94842a314a3434bb7b04bd6ce3a3bd0337f
Lyman <limianfeng12@gmail.com>
2016-08-27 10:12:35 +0800

    添加可移动方块逻辑

---
+++ Assets/Script/SpriteScript/MoveFloorScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveFloorScript : MonoBehaviour {
+
+	private float leftLoc = 10f;
+	private float rightLoc = 13f;
+	private bool isRight = true;
+	private float speed = 0.05f;
+
+	private GameObject marry; //站着的人物
+
+	// Use this for initialization
+	void Start () {
+	
+		marry = null;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (transform.position.x >= this.rightLoc) {
+			transform.position = new Vector2(this.rightLoc, transform.position.y);
+			isRight = false;
+		} else if (transform.position.x <= this.leftLoc) {
+			transform.position = new Vector2(this.leftLoc, transform.position.y);
+			isRight = true;
+		}
+
+		if (isRight && transform.position.x < this.rightLoc) {
+			transform.Translate (new Vector3 (speed, 0, 0));
+			if (marry != null) {
+				marry.transform.Translate (new Vector3 (speed, 0, 0));
+			} 
+		} else if (!isRight && transform.position.x > this.leftLoc) {
+			transform.Translate (new Vector3 (-speed, 0, 0));
+			if (marry != null) {
+				marry.transform.Translate (new Vector3 (-speed, 0, 0));
+			} 
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+		marry = coll.gameObject;
+	}
+
+	void OnCollisionStay2D(Collision2D coll) {
+
+		marry = coll.gameObject;
+	}
+
+	void OnCollisionExit2D(Collision2D coll) {
+
+		marry = null;
+	}
+}
