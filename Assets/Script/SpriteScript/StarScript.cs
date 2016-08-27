d02dc07e5812749ffb0b6441449fbc14d83b62bd
Lyman <limianfeng12@gmail.com>
2016-08-27 09:22:16 +0800

    初始化场景物件

---
+++ Assets/Script/SpriteScript/StarScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StarScript : MonoBehaviour {
+
+	private bool isGet = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (isGet) {
+			transform.Translate (new Vector2(0f, 0.1f));
+		}
+
+		if (Camera.main.WorldToScreenPoint (transform.position).y > Screen.height) {
+
+			transform.Translate (new Vector2(0f, 10f));
+			isGet = false;
+		}
+			
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		isGet = true;
+	}
+
+}
