cdcc9cb1a52a2ed4975308fa5f6184852ef99395
Lyman <limianfeng12@gmail.com>
2016-09-11 18:37:57 +0800

    拼图场景，完成移动逻辑

---
+++ Assets/Script/SpriteScript/jigsaw/JigsawBrickScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class JigsawBrickScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.touchCount > 0) {
+			if (Input.GetTouch (0).phase == TouchPhase.Began) {
+
+				GameObject mainLogic = GameObject.Find("MainLogic");
+
+				Vector2 touchPos = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);
+				Vector2 objPos = this.gameObject.transform.position;
+
+				if ((objPos - touchPos).magnitude < mainLogic.GetComponent<JigsawMainLogicScript> ().getBrickWidth() / 2) {
+
+					int index = mainLogic.GetComponent<JigsawMainLogicScript> ().getNextPostion (gameObject.name);
+
+					if (index >= 0) {
+						transform.position = mainLogic.GetComponent<JigsawMainLogicScript> ().getPostion (index);
+						mainLogic.GetComponent<JigsawMainLogicScript> ().moveBrick (gameObject.name, index);
+					}
+				}
+
+			} else if (Input.GetTouch (0).phase == TouchPhase.Moved) {
+				
+			} else if (Input.GetTouch (0).phase == TouchPhase.Ended) {
+				
+			}
+		}
+	}
+}
--- Assets/Script/SpriteScript/jigsaw/JigsawMainLogicScript.cs
+++ Assets/Script/SpriteScript/jigsaw/JigsawMainLogicScript.cs
@@ -64,5 +64,58 @@ public class JigsawMainLogicScript : MonoBehaviour {
 
 		return list;
 	}
+
+	//获取下一步需要到达的位置，为-1则不可移动
+	public int getNextPostion(string name) {
+
+		int index = -1;
+		for (int i = 0; i < 9; ++i) {
+			if (name.Equals (map [i])) {
+				index = i;
+				break;
+			}
+		}
+			
+		if (index - 3 >= 0 && "".Equals (map [index - 3])) {    //上
+			return index - 3;
+		} else if (index + 3 <= 8 && "".Equals (map [index + 3])) {     //下
+			return index + 3;
+		} else if (index - 1 >= 0 && (index - 1) / 3 == index / 3 && "".Equals (map [index - 1])) {     //左
+			return index - 1;
+		} else if ((index + 1) / 3 == index / 3 && "".Equals (map [index + 1])) {    //右
+			return index + 1;
+		} else {
+			return -1;
+		}
+	}
+
+	//根据序号获取位置
+	public Vector2 getPostion(int index) {
+
+		return this.getPostionWithIndex (index);
+	}
+
+	//获取方块宽度
+	public float getBrickWidth() {
+
+		return this.brickWidth;
+	}
+
+	//将某个方块移到某个位置
+	public void moveBrick(string name, int targetIndex) {
+
+		int index = -1;
+		for (int i = 0; i < 9; ++i) {
+			if (name.Equals (map [i])) {
+				index = i;
+				break;
+			}
+		}
+		if (index >= 0 && map [targetIndex] == "") {
+
+			map [index] = "";
+			map [targetIndex] = name;
+		}
+	}
 }
 
