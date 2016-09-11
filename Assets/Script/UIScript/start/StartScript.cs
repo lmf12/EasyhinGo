11758b6c86878c65898946c3b06184a70b9d8151
Lyman <limianfeng12@gmail.com>
2016-09-11 11:47:33 +0800

    整体框架流程

---
+++ Assets/Script/UIScript/start/StartScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems; 
+using System.Collections;
+
+public class StartScript : MonoBehaviour,IPointerDownHandler,IPointerUpHandler {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+
+	// 当按钮被按下后系统自动调用此方法  
+	public void OnPointerDown (PointerEventData eventData) {
+
+	}  
+
+	// 当按钮抬起的时候自动调用此方法  
+	public void OnPointerUp (PointerEventData eventData) {
+
+		Application.LoadLevel (1);
+	} 
+}
