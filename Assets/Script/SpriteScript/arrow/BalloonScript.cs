54aa6c02daf3b324448dee8c66866e0892d2654f
Lyman <limianfeng12@gmail.com>
2016-09-24 17:05:51 +0800

    初始化气球物体和连接

---
+++ Assets/Script/SpriteScript/arrow/BalloonScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonScript : MonoBehaviour {
+
+	private float g = 9.8f; //重力
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2(0, 2*g));
+	}
+}
