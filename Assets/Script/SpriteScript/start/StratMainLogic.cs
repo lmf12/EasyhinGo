7b42d141ab5b133feb1bc57754d80f952e23f773
Lyman <limianfeng12@gmail.com>
2016-10-23 18:21:21 +0800

    修改应用名称，添加网络请求

---
+++ Assets/Script/SpriteScript/start/StratMainLogic.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StratMainLogic : MonoBehaviour {
+
+	private WWWHelper wwwHelper;
+	public Text title;
+
+	// Use this for initialization
+	void Start () {
+		
+		wwwHelper = GameObject.Find ("HttpHelper").GetComponent<WWWHelper> ();
+
+		wwwHelper.GET ("http://115.29.144.170/risker/", gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void RequestDone (string result) {
+
+		title.text = result;
+	}
+}
---
+++ Assets/Script/ToolScript/WWWHelper.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class WWWHelper : MonoBehaviour 
+{
+	private static WWWHelper instance = null;
+
+	private WWW m_www;
+	private GameObject m_CallBackTarget = null;
+	private bool m_bIsBeginRequest = false;
+	private bool m_bIsDone = true;
+
+	public bool IsBeginRequest
+	{
+		get { return m_bIsBeginRequest; }
+		set { m_bIsBeginRequest = value; }
+	}
+
+	public bool IsDone
+	{
+		get { return m_bIsDone; }
+		set { m_bIsDone = value; }
+	}
+
+	public static WWWHelper Instance
+	{
+		get { return instance; }
+	}
+
+	void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+		}
+		else
+		{
+			instance = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	public void GET(string url, GameObject callBackTarget)
+	{
+		if (m_bIsDone)
+		{
+			m_CallBackTarget = callBackTarget;
+			this.m_www = new WWW(url);
+			m_bIsBeginRequest = true;
+			m_bIsDone = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (m_bIsBeginRequest)
+		{
+			if (this.m_www.isDone)
+			{
+				if (null != m_CallBackTarget)
+					m_CallBackTarget.SendMessage("RequestDone",this.m_www.text);
+				m_bIsDone = true;
+				m_bIsBeginRequest = false;
+			}
+		}
+	}
+}
