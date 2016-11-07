f990e9d95852185437092408f721d5408a41df7b
Lyman <limianfeng12@gmail.com>
2016-11-07 22:28:45 +0800

    启动页

--- Assets/Script/SpriteScript/start/StratMainLogic.cs
+++ Assets/Script/SpriteScript/start/StratMainLogic.cs
@@ -4,24 +4,58 @@ using UnityEngine.UI;
 
 public class StratMainLogic : MonoBehaviour {
 
+	public Image launch;
+
 	private WWWHelper wwwHelper;
-//	public Text title;
+
+	private float duration = 1.0f;  //动画持续
+	private float thinSpead;
+	private bool isPlaying = false;  //是否播放动画
 
 	// Use this for initialization
 	void Start () {
-		
+
+		Invoke("hideLaunch", 2);
+		thinSpead = (launch.color.a - 0) / (duration / Time.fixedDeltaTime);
+
+
 		wwwHelper = GameObject.Find ("HttpHelper").GetComponent<WWWHelper> ();
 
 		wwwHelper.GET ("http://115.29.144.170/risker/", gameObject);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 	
+		if (isPlaying) {
+
+			if (Mathf.Abs (0 - launch.color.a) <= thinSpead) {
+
+				Color c = launch.color; 
+				c.a = 0; 
+				launch.color = c;
+			} else {
+
+				Color c = launch.color; 
+				c.a -= thinSpead; 
+				launch.color = c;
+			}
+
+			if (launch.color.a == 0) {
+
+				launch.transform.localScale = new Vector2 (0, 0);
+				isPlaying = false;
+			}
+		}
+
 	}
 
 	void RequestDone (string result) {
 
-//		title.text = result;
+	}
+
+	void hideLaunch() {
+
+		isPlaying = true;
 	}
 }
