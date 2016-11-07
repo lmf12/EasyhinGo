f57eee873385f604793fd391deabc5d5e7dda60c
Lyman <limianfeng12@gmail.com>
2016-11-07 23:39:04 +0800

    问答顶部UI

---
+++ Assets/Script/ToolScript/TitleBgScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleBgScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		transform.position = new Vector2 (Screen.width / 2, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
