9f7822eaf10e8d55ad58d6a20e3ad9015c400581
Lyman <limianfeng12@gmail.com>
2016-11-14 00:25:09 +0800

    选择界面，修改滑动实现方式

--- Assets/Script/SpriteScript/switch/SwitchMainLogic.cs
+++ Assets/Script/SpriteScript/switch/SwitchMainLogic.cs
@@ -24,6 +24,8 @@ public class SwitchMainLogic : MonoBehaviour {
 	private Vector2 touchLastPos;
 	private float moveDistance;
 
+	public Image mainLoc;
+
 	bool isScrolling = false;
 
 	private float baseVelocity = 12.0f;
@@ -38,80 +40,21 @@ public class SwitchMainLogic : MonoBehaviour {
 	private float beginX4 = 0.01f;
 	private float endX4 = -70.0f;
 
+
+	private float originLocX;
+	private float originMainX;
+
 	// Use this for initialization
 	void Start () {
 
+		originLocX = background1.transform.position.x;
+		originMainX = Camera.main.ScreenToWorldPoint (mainLoc.transform.position).x;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-
-		if (isScrolling) {
-			if (background1.transform.position.x <= endX1 + minDis) {
-
-				background1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
-				background1.transform.position = new Vector2 (endX1, background1.transform.position.y);
-				isScrolling = false;
-			} else if (background1.transform.position.x >= beginX1 - minDis) {
-
-				background1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
-				background1.transform.position = new Vector2 (beginX1, background1.transform.position.y);
-				isScrolling = false;
-			}
-
-			if (background1.GetComponent<Rigidbody2D> ().velocity.x > 0) {
-				if (background1.GetComponent<Rigidbody2D> ().velocity.x - baseForce > 0) {
-					background1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (background1.GetComponent<Rigidbody2D> ().velocity.x - baseForce, 0);
-				}
-				else {
-					background1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
-					isScrolling = false;
-				}
-			} else if (background1.GetComponent<Rigidbody2D> ().velocity.x < 0) {
-				if (background1.GetComponent<Rigidbody2D> ().velocity.x + baseForce < 0) {
-					background1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (background1.GetComponent<Rigidbody2D> ().velocity.x + baseForce, 0);
-				}
-				else {
-					background1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
-					isScrolling = false;
-				}
-			}
-		}
-
-		if (Input.touchCount > 0) {
-			if (Input.GetTouch (0).phase == TouchPhase.Began) {
-				
-				touchLastPos = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);
-				background1.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
-				isScrolling = false;
-			} else if (Input.GetTouch (0).phase == TouchPhase.Moved) {
-
-				Vector2 touchCurrentPos = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);
-				moveDistance = touchCurrentPos.x - touchLastPos.x;
-
-				float afterPosX = background1.transform.position.x + moveDistance;
-				if (afterPosX <= endX1) {
-					background1.transform.position = new Vector2 (endX1, background1.transform.position.y);
-				} else if (afterPosX >= beginX1) {
-					background1.transform.position = new Vector2 (beginX1, background1.transform.position.y);
-				} else {
-					background1.transform.Translate (new Vector3(moveDistance, 0, 0));
-				}
-				touchLastPos = touchCurrentPos;
-			} else if (Input.GetTouch (0).phase == TouchPhase.Ended) {
-
-
-				if (moveDistance != 0 && background1.transform.position.x - endX1 >= minDis && beginX1 - background1.transform.position.x >= minDis) {
-					background1.GetComponent<Rigidbody2D> ().velocity = new Vector2(baseVelocity * moveDistance, 0);
-					isScrolling = true;
-				}
-			}
-		}
-
-
-	}
-
 	void Update() {
+
+		updateBackground1Pos ();
 
 		updateBackground2Pos ();
 		updateBackground3Pos ();
@@ -121,6 +64,13 @@ public class SwitchMainLogic : MonoBehaviour {
 		updateBoat ();
 
 		updateButtonLoc ();
+	}
+
+	private void updateBackground1Pos() {
+
+		float posX = Camera.main.ScreenToWorldPoint (mainLoc.transform.position).x;
+		float background1X = originLocX + posX - originMainX;
+		background1.transform.position = new Vector2 (background1X, background1.transform.position.y);
 	}
 
 	private void updateBackground2Pos() {
