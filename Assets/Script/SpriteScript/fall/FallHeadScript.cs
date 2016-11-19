d1fa3f1c78d5d122a1b2da3bc16fd4c5d1d349db
Lyman <limianfeng12@gmail.com>
2016-11-19 16:56:58 +0800

    随机下落逻辑

---
+++ Assets/Script/SpriteScript/fall/FallHeadScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallHeadScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+
+		GameObject mainLogic = GameObject.Find ("FallMainLogic");
+
+		if (other.gameObject.name.Equals ("people_1")) {
+
+			if (gameObject.name.Equals ("MonoPrefab(Clone)")) {
+				mainLogic.GetComponent<FallMainLogicScript> ().getMono ();
+			} else if (gameObject.name.Equals ("RayPrefab(Clone)")) {
+				mainLogic.GetComponent<FallMainLogicScript> ().getRay ();
+			} else if (gameObject.name.Equals ("RocPrefab(Clone)")) {
+				mainLogic.GetComponent<FallMainLogicScript> ().getRoc ();
+			} else if (gameObject.name.Equals ("SongPrefab(Clone)")) {
+				mainLogic.GetComponent<FallMainLogicScript> ().getSong ();
+			}
+		}
+
+		Destroy(gameObject);
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+
+	}
+}
--- Assets/Script/SpriteScript/fall/FallMainLogicScript.cs
+++ Assets/Script/SpriteScript/fall/FallMainLogicScript.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FallMainLogicScript : MonoBehaviour {
 
 	public GameObject people;
 	public GameObject boom;
+
+	public GameObject ray;
+	public GameObject roc;
+	public GameObject song;
+	public GameObject mono;
 
 	private float speed = 0.06f;
 
@@ -17,6 +23,28 @@ public class FallMainLogicScript : MonoBehaviour {
 	private bool isLeftPress;
 	private bool isRightPress;
 
+
+	private GameObject currentObj1;
+	private GameObject currentObj2;
+	private GameObject currentObj3;
+	private GameObject currentObj4;
+
+	private int objType;
+
+
+
+	public Image imgRay;
+	public Image imgMono;
+	public Image imgSong;
+	public Image imgRoc;
+
+	public Sprite spRay;
+	public Sprite spMono;
+	public Sprite spSong;
+	public Sprite spRoc;
+
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -25,16 +53,30 @@ public class FallMainLogicScript : MonoBehaviour {
 
 		isLeftPress = false;
 		isRightPress = false;
-
-		createBoom (Random.Range(minX, maxX));
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-	
-		if (GameObject.Find ("BoomPerfab(Clone)") == null) {
 
-			createBoom (Random.Range(minX, maxX));
+		float flag = canCreate ();
+		if (flag > -8) {
+
+			switch (objType) {
+			case 1:
+				currentObj1 = createObj (flag);
+				break;
+			case 2:
+				currentObj2 = createObj (flag);
+				break;
+			case 3:
+				currentObj3 = createObj (flag);
+				break;
+			case 4:
+				currentObj4 = createObj (flag);
+				break;
+			default:
+				break;
+			}
 		}
 
 		if (isMoving) {
@@ -106,8 +148,82 @@ public class FallMainLogicScript : MonoBehaviour {
 		people.transform.localScale = scale;
 	}
 
-	private void createBoom(float x) {
+	private GameObject createBoom(float x) {
 
-		GameObject fire = (GameObject)Instantiate (boom, new Vector2(x, 5), Quaternion.identity);
+		return (GameObject)Instantiate (boom, new Vector2(x, 5), Quaternion.identity);
+	}
+
+	private GameObject createHead(float x) {
+
+		int index = (int)(Random.value * 9);
+
+		switch (index) {
+		case 0:
+		case 1:
+			return (GameObject)Instantiate (ray, new Vector2(x, 5), Quaternion.identity);
+			break;
+		case 2:
+			int i = (int)(Random.value * 2);
+			return (GameObject)Instantiate (i==0 ?song:mono, new Vector2(x, 5), Quaternion.identity);
+			break;
+		case 3:
+		case 4:
+		case 5:
+			return (GameObject)Instantiate (mono, new Vector2(x, 5), Quaternion.identity);
+			break;
+		default:
+			return (GameObject)Instantiate (roc, new Vector2(x, 5), Quaternion.identity);
+			break;
+		}
+	}
+
+	private GameObject createObj(float x) {
+
+		int index = (int)(Random.value * 3);
+
+		if (index == 0) {
+			return createBoom (x);
+		} else {
+			return createHead (x);
+		}
+	}
+
+	private float canCreate () {
+
+		if (currentObj1 == null) {
+			objType = 1;
+			return (-5.5f + Random.value * 2);
+		} else if (currentObj2 == null) {
+			objType = 2;
+			return (-2.5f + Random.value * 2);
+		} else if (currentObj3 == null) {
+			objType = 3;
+			return (0.5f + Random.value * 2);
+		} else if (currentObj4 == null) {
+			objType = 4;
+			return (3.5f + Random.value * 2);
+		} else {
+			return -10.0f;
+		}
+	}
+
+	public void getMono() {
+
+		imgMono.sprite = spMono;
+	}
+
+	public void getRoc() {
+
+		imgRoc.sprite = spRoc;
+	}
+
+	public void getRay() {
+
+		imgRay.sprite = spRay;
+	}
+
+	public void getSong() {
+
+		imgSong.sprite = spSong;
 	}
 }
